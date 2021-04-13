55329181f21ffa787857d1251bde64d6661fbb59
Michael Brewer <michael.brewer@gyft.com>
2021-04-13 09:03:49 -0700

    feat: AppSync resolver support

--- src/f1/app.py
+++ src/f1/app.py
@@ -1,34 +1,39 @@
 import asyncio
-import json
-from typing import Any, Dict, Union
+from typing import Any, Dict, List
 
 from aws_lambda_powertools import Logger, Tracer
+from aws_lambda_powertools.event_handler import AppSyncResolver
 from aws_lambda_powertools.utilities.typing import LambdaContext
 
 from .f1_client import F1Client
 
 logger = Logger()
 tracer = Tracer()
+f1_client = F1Client()
+app = AppSyncResolver()
 
 
-def build_response(status: int, context_type: str, result: Union[str, dict, list]) -> Dict[str, Any]:
-    body = result if isinstance(result, str) else json.dumps(result)
-    return {
-        "statusCode": status,
-        "headers": {"Content-Type": context_type},
-        "body": body,
-    }
+@app.resolver(type_name="Query", field_name="listResults")
+async def race_results(season: str = "current", race: str = "last") -> List:
+    data = await f1_client.race_results(season, race)
+    logger.debug(data)
+    return data["MRData"]["RaceTable"]["Races"][0]["Results"]
+
+
+@app.resolver(type_name="Query", field_name="listSchedule")
+async def schedule(season: str = "current") -> List:
+    data = await f1_client.schedule(season)
+    logger.debug(data)
+    return data["MRData"]["RaceTable"]["Races"]
+
+
+@app.resolver(type_name="Query", field_name="summaryResults")
+async def summary_results(season: str = "current", number_races: str = "17") -> List:
+    return await f1_client.summary_results(season, int(number_races))
 
 
 @logger.inject_lambda_context(log_event=True)
 @tracer.capture_lambda_handler(capture_error=True, capture_response=True)
 def lambda_handler(event: Dict[str, Any], context: LambdaContext) -> Dict[str, Any]:
     logger.debug("Testing debug message...")
-    f1_client = F1Client()
-    data = asyncio.run(f1_client.race_results())
-    logger.debug(data)
-    return build_response(
-        200,
-        "application-json",
-        data["MRData"]["RaceTable"]["Races"][0]["Results"],
-    )
+    return asyncio.run(app.resolve(event, context))
--- src/f1/f1_client.py
+++ src/f1/f1_client.py
@@ -1,9 +1,20 @@
 import asyncio
-from typing import Dict
+from typing import Dict, List, Any
 
 import aiohttp
 from aiohttp import ClientTimeout
 from aws_lambda_powertools.tracing import aiohttp_trace_config
+
+
+def print_driver_lap(driver):
+    return f'{driver["Driver"]["givenName"]} {driver["Driver"]["familyName"]} - {driver["FastestLap"]["Time"]["time"]}'
+
+
+def print_driver(driver, first: bool = False):
+    return (
+        f'{driver["Driver"]["givenName"]} {driver["Driver"]["familyName"]} {driver["points"]} pts '
+        f'{"" if first else driver["Time"]["time"] + " secs"}'
+    )
 
 
 class F1Client:
@@ -39,3 +50,33 @@ class F1Client:
     async def fetch_race(self, session, season: str, race: str) -> Dict:
         async with session.get(f"{self.base_url}/{season}/{race}/results.json") as response:
             return await response.json()
+
+    async def summary_results(self, season: str, race_no: int):
+        races = await self.race_results_for_season(season, race_no)
+        data: List = []
+
+        for race in races:
+            race = race["MRData"]["RaceTable"]["Races"][0]
+            results = race["Results"]
+            first = results[0]
+            second = results[1]
+            third = results[2]
+
+            fastest_lap: str = ""
+            for result in results:
+                if "FastestLap" in result and result["FastestLap"]["rank"] == "1":
+                    fastest_lap = print_driver_lap(result)
+                    break
+
+            data.append(
+                {
+                    "date": race["date"],
+                    "raceName": race["raceName"],
+                    "1st": print_driver(first, True),
+                    "2nd": print_driver(second),
+                    "3rd": print_driver(third),
+                    "fastestLap": fastest_lap,
+                }
+            )
+
+        return data
